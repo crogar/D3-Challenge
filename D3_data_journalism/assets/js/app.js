41fd2bd17c09e532ac121d0988c93e2138c2f87d
crogar <58400421+crogar@users.noreply.github.com>
2021-05-09 20:42:44 -0400

    Update app.js
    added on-resize event

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,10 @@
-// @TODO: YOUR CODE HERE!
+function makeResponsive() {
+
+ 
+}
+
+// When the browser loads, makeResponsive() is called.
+makeResponsive();
+
+// When the browser window is resized, makeResponsive() is called.
+d3.select(window).on("resize", makeResponsive);
